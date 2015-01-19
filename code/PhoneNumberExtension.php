ceda216b796a507a08cb9b9ce822b19622d5962d
Thomas Portelange <thomas.portelange@boomerang.be>
2015-01-19 12:00:52 +0100

    remove third party in favour of composer

--- code/PhoneNumberExtension.php
+++ code/PhoneNumberExtension.php
@@ -1,24 +1,34 @@
 <?php
 
 /**
- * LibPhoneNumberExtension
+ * PhoneNumberExtension
  *
  * @author lekoala
  */
-class LibPhoneNumberExtension extends DataExtension {
+class PhoneNumberExtension extends DataExtension
+{
+    private static $db = array(
+        'CountryCode' => 'Varchar(2)',
+        'Phone' => 'Varchar(255)',
+        'MobilePhone' => 'Varchar(255)',
+    );
 
-	private static $db = array(
-		'CountryCode' => 'Varchar(2)',
-		'PhoneNumber' => 'Varchar(255)',
-	);
+    public function updateCMSFields(FieldList $fields)
+    {
 
-	public function updateCMSFields(FieldList $fields) {
+        $fields->AddFieldToTab('Root.Main',
+            new CountryDropdownField('CountryCode',
+            _t('LibPhoneNumberExtension.CountryCode', 'Country')));
+        $fields->AddFieldToTab('Root.Main',
+            $phonefield       = new LibPhoneNumberField('Phone',
+            _t('LibPhoneNumberExtension.PhoneNumber', 'Phone')));
+        $fields->AddFieldToTab('Root.Main',
+            $mobilephonefield = new LibPhoneNumberField('MobilePhone',
+            _t('LibPhoneNumberExtension.MobilePhoneNumber', 'Mobile Phone')));
 
-		$fields->AddFieldToTab('Root.Main', new CountryDropdownField('CountryCode', _t('LibPhoneNumberExtension.CountryCode','Country')));
-		$fields->AddFieldToTab('Root.Main', $phonefield = new LibPhoneNumberField('PhoneNumber', _t('LibPhoneNumberExtension.PhoneNumber','Phone Number')));
-		$phonefield->setCountryField('CountryCode');
-		
-		return $fields;
-	}
+        $phonefield->setCountryField('CountryCode');
+        $mobilephonefield->setCountryField('CountryCode');
 
+        return $fields;
+    }
 }
