46751f1104cfc28b8a30435532c6fc2d9e293885
Thomas <thomas@lekoala.be>
2015-09-16 16:55:25 +0200

    update defaults

--- code/PhoneNumberExtension.php
+++ code/PhoneNumberExtension.php
@@ -9,8 +9,8 @@ class PhoneNumberExtension extends DataExtension
 {
     private static $db = array(
         'CountryCode' => 'Varchar(2)',
-        'Phone' => 'Varchar(255)',
-        'MobilePhone' => 'Varchar(255)',
+        'PhoneNumber' => 'Varchar(255)',
+        'MobilePhoneNumber' => 'Varchar(255)',
     );
 
     /**
@@ -23,7 +23,7 @@ class PhoneNumberExtension extends DataExtension
      */
     public static function twilioLookup($phone, $country)
     {
-        if(strlen($country) != 2) {
+        if (strlen($country) != 2) {
             throw new Exception("Country code must be 2 characters long");
         }
         $username = TWILIO_ACCOUNT_SID;
@@ -41,18 +41,18 @@ class PhoneNumberExtension extends DataExtension
 
     public function updateCMSFields(FieldList $fields)
     {
-
         $fields->AddFieldToTab('Root.Main',
             new CountryDropdownField('CountryCode',
             _t('LibPhoneNumberExtension.CountryCode', 'Country')));
-        $fields->AddFieldToTab('Root.Main',
-            $phonefield       = new LibPhoneNumberField('Phone',
-            _t('LibPhoneNumberExtension.PhoneNumber', 'Phone')));
-        $fields->AddFieldToTab('Root.Main',
-            $mobilephonefield = new LibPhoneNumberField('MobilePhone',
-            _t('LibPhoneNumberExtension.MobilePhoneNumber', 'Mobile Phone')));
 
+        $fields->AddFieldToTab('Root.Main',
+            $phonefield = new LibPhoneNumberField('PhoneNumber',
+            _t('LibPhoneNumberExtension.PhoneNumber', 'Phone')));
         $phonefield->setCountryField('CountryCode');
+
+        $fields->AddFieldToTab('Root.Main',
+            $mobilephonefield = new LibPhoneNumberField('MobilePhoneNumber',
+            _t('LibPhoneNumberExtension.MobilePhoneNumber', 'Mobile Phone')));
         $mobilephonefield->setCountryField('CountryCode');
 
         return $fields;
